3f4610925e5be4493df456a92142a2c6d8e5d0c0
github username <github email address>
2020-01-14 10:06:17 -0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+post = Post.create([{title: 'blog1', description: 'This is my blog'}, {title: 'blog2', description: 'This is my blog'}, {title: 'blog3', description: 'This is my blog'}])
